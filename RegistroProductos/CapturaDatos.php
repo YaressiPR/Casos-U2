a0f987bbab494f7a9d36e43e8cb6bfdd554f5dd6
YaressiPR <167457168+YaressiPR@users.noreply.github.com>
2024-05-04 00:02:13 -0600

    Add files via upload

---
+++ RegistroProductos/CapturaDatos.php
@@ -0,0 +1,14 @@
+<?php 
+function getDescripcion() 
+{ 
+    return $_POST['txtDescripcion']; 
+} 
+function getStock() 
+{ 
+    return $_POST['txtStock']; 
+} 
+function getPrecio() 
+{ 
+    return $_POST['txtPrecio']; 
+} 
+?> 
