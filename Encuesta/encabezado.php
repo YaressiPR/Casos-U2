4a0ab400e67b3e2f9b7e0a0341149d4421ae850b
YaressiPR <167457168+YaressiPR@users.noreply.github.com>
2024-05-03 23:45:26 -0600

    Add files via upload

---
+++ Encuesta/encabezado.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link href="estilo.css" rel="stylesheet">
+</head>
+<body>
+    <img src="banner_seguridad.jpeg" width="1350" height="240"/>
+    <h3 id="centrado">ENCUESTA DE INSEGURIDAD DEL CIUDADANO
+                      EN LA CIUDAD DE LIMA
+    </h3>
+</body>
+</html>
